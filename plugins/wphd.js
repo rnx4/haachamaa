1d86298e00702b034f814250a3d64a25cc229836
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-26 22:01:28 +0800

    Create wphd.js

---
+++ plugins/wphd.js
@@ -0,0 +1,22 @@
+let fetch = require("node-fetch")
+let handler = async (m, { conn }) => {
+await m.reply(global.wait)
+let res = await fetch('https://raw.githubusercontent.com/unx21/ngetezz/main/src/data/tezztt.txt')
+let txt = await res.text()
+
+let arr = txt.split('\n')
+let cita = arr[Math.floor(Math.random() * arr.length)]
+conn.sendFile(m.chat, cita, '.png', '*©ChamaaChamaa*', m) 
+}
+
+
+handler.command = /^wphd$/i
+handler.limit = false
+handler.owner = true
+handler.register = true
+module.exports = handler
+//Made with 💙 by unx//
+
+
+
+module.exports = handler
