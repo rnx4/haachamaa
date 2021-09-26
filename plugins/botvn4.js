47d3efe8d6be7aeb2819bfce0b19bb88cf721d30
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-27 00:29:31 +0800

    Update botvn4.js

--- plugins/botvn4.js
+++ plugins/botvn4.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Bro|Bruh|bro|bruh/
+handler.customPrefix = /Bro|Bruh|bruh/
 handler.command = new RegExp
 module.exports = handler
