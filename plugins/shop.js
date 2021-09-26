910d9dc0c66260236046545ca92341a6f4e35682
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-26 23:05:28 +0800

    Update shop.js

--- plugins/shop.js
+++ plugins/shop.js
@@ -381,8 +381,6 @@ Sampah:     ${Ssampah}\n\n
     }
 }
 
-handler.help = ['shop <sell|buy> <args>', 'toko <sell|buy> <args>']
-handler.tags = ['rpg']
 handler.register = true
     
 handler.command = /^(shop|toko|buy|beli|sell|jual)$/i
