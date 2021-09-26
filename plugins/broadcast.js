55e7b67a82173abe7e799687c5e910241895a3e6
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-26 23:52:32 +0800

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,11 +1,10 @@
-let handler = async (m, { conn, text }) => {
-  if (conn.user.jid === global.conn.user.jid) {
-    let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
-    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
-    for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
-${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
-  } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
+let handler  = async (m, { conn, text }) => {
+  let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
+  let content = (/bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
+  for (let id of chats) conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? {
+    contextInfo: m.msg.contextInfo
+  } : {})
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
 handler.command = /^(broadcast|bc)$/i
 handler.owner = true
