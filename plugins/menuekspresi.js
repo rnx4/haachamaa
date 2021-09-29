6c7a7a527d15c4ac8693d626c3da168d3a474f9e
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-30 00:39:05 +0800

    Update menuekspresi.js

--- plugins/menuekspresi.js
+++ plugins/menuekspresi.js
@@ -6,21 +6,34 @@ let teks = `
 ┊      Ⓛ  = limit
 ┊
 ┊
+┊ ⍚ _.awoo_  Ⓛ
+┊ ⍚ _.blush_  Ⓛ
 ┊ ⍚ _.bonk_  [ @user ] Ⓛ
+┊ ⍚ _.bums_  Ⓛ
+┊ ⍚ _.cringe_  Ⓛ
 ┊ ⍚ _.cry_  Ⓛ
 ┊ ⍚ _.cuddle_  Ⓛ
 ┊ ⍚ _.facepalm_  Ⓛ
+┊ ⍚ _.glomp_  Ⓛ
+┊ ⍚ _.happy_  Ⓛ
+┊ ⍚ _.highfive_  Ⓛ
 ┊ ⍚ _.hug_  Ⓛ
+┊ ⍚ _.kick_  Ⓛ
 ┊ ⍚ _.kiss_  Ⓛ
 ┊ ⍚ _.kitsune_  Ⓛ
+┊ ⍚ _.nom_  Ⓛ
 ┊ ⍚ _.notstonk_  Ⓛ
 ┊ ⍚ _.nyan_  Ⓛ
 ┊ ⍚ _.pat_  Ⓛ
+┊ ⍚ _.poke_  Ⓛ
 ┊ ⍚ _.punch_  Ⓛ
 ┊ ⍚ _.slap_  Ⓛ
+┊ ⍚ _.smile_  Ⓛ
 ┊ ⍚ _.smug_  Ⓛ
 ┊ ⍚ _.stonk_  Ⓛ
+┊ ⍚ _.wave_  Ⓛ
 ┊ ⍚ _.wink_  Ⓛ
+┊ ⍚ _.yeet_  Ⓛ
 ╰────────
 `.trim()
   conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Menu Sticker Wibu', 'status@broadcast')
