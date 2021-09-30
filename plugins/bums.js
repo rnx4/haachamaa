9d871d0871bebac71d53e96089f0dbddcb23225e
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-30 19:58:00 +0800

    Create bums.js

---
+++ plugins/bums.js
@@ -0,0 +1,24 @@
+let fetch = require("node-fetch")
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn}) => {
+await m.reply(global.wait)
+  try {
+  let res = await fetch('https://api.waifu.pics/sfw/bonk')
+  let json = await res.json()
+  let { 
+url
+} = json
+let stiker = await sticker(null, url, 'Bumss', 'Haachamaa')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+  }
+}
+handler.command = /^bums/i
+handler.register = true
+handler.limit = true
+
+module.exports = handler
