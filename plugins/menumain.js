8183d2d4daa656db59ab726b65b99453763f0d32
unxzx <84166927+unx21@users.noreply.github.com>
2021-10-16 19:31:43 +0800

    Update menumain.js

--- plugins/menumain.js
+++ plugins/menumain.js
@@ -9,28 +9,28 @@ let teks = `
 │  Ⓟ  = Fitur premium
 │
 │
-│ - _.afk_  [ alasan ]
-│ - _.buy_
-│ - _.buy_  [ jumlah limit ]
-│ - _.buyall_
-│ - _.claim_ Ⓗ
-│ - _.daily_ Ⓗ
-│ - _.gift_ Ⓟ Ⓙ
-│ - _.leaderboard_  [ jumlah user ]
-│ - _.lb_  [ jumlah user ]
-│ - _.levelup_
-│ - _.limit_  [ @user mountt ]
-│ - _.paylimit_  [ @user mountt ]
-│ - _.mining_ Ⓙ
-│ - _.profile_
-│ - _.rank_
-│ - _.rankcard_
-│ - _.daftar_  [ nama|umur ]
-│ - _.register_  [ nama|umur ]
-│ - _.tf_  [ uang @user ] Ⓛ
-│ - _.transfer_  [ uang @user ]
-│ - _.unreg_  [ SN|Serial number ]
-│ - _.unregister_  [ SN|Serial number ]
+│ - _${usedPrefix}afk_  [ alasan ]
+│ - _${usedPrefix}buy_
+│ - _${usedPrefix}buy_  [ jumlah limit ]
+│ - _${usedPrefix}buyall_
+│ - _${usedPrefix}claim_ Ⓗ
+│ - _${usedPrefix}daily_ Ⓗ
+│ - _${usedPrefix}gift_ Ⓟ Ⓙ
+│ - _${usedPrefix}leaderboard_  [ jumlah user ]
+│ - _${usedPrefix}lb_  [ jumlah user ]
+│ - _${usedPrefix}levelup_
+│ - _${usedPrefix}limit_  [ @user mountt ]
+│ - _${usedPrefix}paylimit_  [ @user mountt ]
+│ - _${usedPrefix}mining_ Ⓙ
+│ - _${usedPrefix}profile_
+│ - _${usedPrefix}rank_
+│ - _${usedPrefix}rankcard_
+│ - _${usedPrefix}daftar_  [ nama|umur ]
+│ - _${usedPrefix}register_  [ nama|umur ]
+│ - _${usedPrefix}tf_  [ uang @user ] Ⓛ
+│ - _${usedPrefix}transfer_  [ uang @user ]
+│ - _${usedPrefix}unreg_  [ SN|Serial number ]
+│ - _${usedPrefix}unregister_  [ SN|Serial number ]
 ╰──────
 `.trim()
   conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Menu Utama', 'status@broadcast')
