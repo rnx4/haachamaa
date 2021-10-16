6691018060be7ee0bb42eb417c3433e8992e0c04
unxzx <84166927+unx21@users.noreply.github.com>
2021-10-16 19:46:22 +0800

    Update menufun.js

--- plugins/menufun.js
+++ plugins/menufun.js
@@ -1,4 +1,4 @@
-let handler = async (m) => {
+let handler  = async (m, { conn, usedPrefix }) => {
   
 let teks = `
 ┌─ *〔 Fun Menu 〕*
@@ -6,81 +6,78 @@ let teks = `
 │     Ⓛ  = limit
 │
 │
-├ ▪ _.apakah_  [ teks ]
-├ ▪ _.apakah_  [ pertanyaan ]
-├ ▪ _.bisakah_  [ teks ]
-├ ▪ _.bisakah_  [ pertanyaan ]
-├ ▪ _.kapan + kah_  [ teks ]
-├ ▪ _.kapan + kah_  [ pertanyaan ]
-├ ▪ _.siapa + kah_  [ teks ]
-├ ▪ _.siapa + kah_  [ pertanyaan ]
-├ ▪ _.artimimpi_  [ teks ] Ⓛ
-├ ▪ _.artinama_  [ nama ] Ⓛ
-├ ▪ _.kbbi_  [ teks ] Ⓛ
-├ ▪ _.zodiak_  [ nama|tgl-bln-thn ] Ⓛ
-├ ▪ _.bass_  [ reply audio ] Ⓛ
-├ ▪ _.blown_  [ reply audio ] Ⓛ
-├ ▪ _.deep_  [ reply audio ] Ⓛ
-├ ▪ _.earrape_  [ reply audio ] Ⓛ
-├ ▪ _.fast_  [ reply audio ] Ⓛ
-├ ▪ _.fat_  [ reply audio ] Ⓛ
-├ ▪ _.nightcore_  [ reply audio ] Ⓛ
-├ ▪ _.reverse_  [ reply audio ] Ⓛ
-├ ▪ _.robot_  [ reply audio ] Ⓛ
-├ ▪ _.slow_  [ reply audio ] Ⓛ
-├ ▪ _.smooth_  [ reply audio ] Ⓛ
-├ ▪ _.tupai_  [ reply audio ] Ⓛ
-├ ▪ _.tomp3_  [ reply media ] Ⓛ
-├ ▪ _.tovn_  [ reply media ] Ⓛ
-├ ▪ _.alay_  [ teks ]
-├ ▪ _.halah_
-├ ▪ _.hilih_
-├ ▪ _.huluh_
-├ ▪ _.heleh_
-├ ▪ _.holoh_
-├ ▪ _.bucincek_
-├ ▪ _.cantikcek_
-├ ▪ _.furrycek_
-├ ▪ _.gabutcek_
-├ ▪ _.gantengcek_
-├ ▪ _.gaycek_
-├ ▪ _.gilacek_
-├ ▪ _.halucek_
-├ ▪ _.hornycek_
-├ ▪ _.jonescek_
-├ ▪ _.lesbicek_
-├ ▪ _.pedocek_
-├ ▪ _.pintarcek_
-├ ▪ _.sadboycek_
-├ ▪ _.sadgirlcek_
-├ ▪ _.stresscek_
-├ ▪ _.tololcek_
-├ ▪ _.wibucek_
-├ ▪ _.jadian_
-├ ▪ _.mention_  [ user ]
-├ ▪ _.say_  [ teks ]
-├ ▪ _.simi_  [ teks ]
-├ ▪ _.simsimi_  [ teks ]
-├ ▪ _.simih_  [ teks ]
-├ ▪ _.bacot_  [ user ]
-├ ▪ _.bucin_
-├ ▪ _.motivasi_
-├ ▪ _.pantun_
-├ ▪ _.puisi_
-├ ▪ _.syair_
-├ ▪ _.quotes_
-├ ▪ _.getaudio_  [ teks ]
-├ ▪ _.getimg_  [ teks ]
-├ ▪ _.getmsg_  [ teks ]
-├ ▪ _.getsticker_  [ teks ]
-├ ▪ _.getvideo_  [ teks ]
-├ ▪ _.getvn_  [ teks ]
-├ ▪ _.listaudio_  [ teks ]
-├ ▪ _.listimg_  [ teks ]
-├ ▪ _.listmsg_  [ teks ]
-├ ▪ _.liststicker_  [ teks ]
-├ ▪ _.listvideo_  [ teks ]
-├ ▪ _.listvn_  [ teks ]
+├ ▪ _${usedPrefix}apakah_  [ teks ]
+├ ▪ _${usedPrefix}apakah_  [ pertanyaan ]
+├ ▪ _${usedPrefix}bisakah_  [ teks ]
+├ ▪ _${usedPrefix}bisakah_  [ pertanyaan ]
+├ ▪ _${usedPrefix}kapan + kah_  [ teks ]
+├ ▪ _${usedPrefix}kapan + kah_  [ pertanyaan ]
+├ ▪ _${usedPrefix}siapa + kah_  [ teks ]
+├ ▪ _${usedPrefix}siapa + kah_  [ pertanyaan ]
+├ ▪ _${usedPrefix}artimimpi_  [ teks ] Ⓛ
+├ ▪ _${usedPrefix}artinama_  [ nama ] Ⓛ
+├ ▪ _${usedPrefix}kbbi_  [ teks ] Ⓛ
+├ ▪ _${usedPrefix}zodiak_  [ nama|tgl-bln-thn ] Ⓛ
+├ ▪ _${usedPrefix}bass_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}blown_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}deep_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}earrape_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}fast_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}fat_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}nightcore_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}reverse_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}robot_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}slow_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}smooth_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}tupai_  [ reply audio ] Ⓛ
+├ ▪ _${usedPrefix}tomp3_  [ reply media ] Ⓛ
+├ ▪ _${usedPrefix}tovn_  [ reply media ] Ⓛ
+├ ▪ _${usedPrefix}alay_  [ teks ]
+├ ▪ _${usedPrefix}halah_
+├ ▪ _${usedPrefix}hilih_
+├ ▪ _${usedPrefix}huluh_
+├ ▪ _${usedPrefix}heleh_
+├ ▪ _${usedPrefix}holoh_
+├ ▪ _${usedPrefix}bucincek_
+├ ▪ _${usedPrefix}cantikcek_
+├ ▪ _${usedPrefix}furrycek_
+├ ▪ _${usedPrefix}gabutcek_
+├ ▪ _${usedPrefix}gantengcek_
+├ ▪ _${usedPrefix}gaycek_
+├ ▪ _${usedPrefix}gilacek_
+├ ▪ _${usedPrefix}halucek_
+├ ▪ _${usedPrefix}hornycek_
+├ ▪ _${usedPrefix}jonescek_
+├ ▪ _${usedPrefix}lesbicek_
+├ ▪ _${usedPrefix}pedocek_
+├ ▪ _${usedPrefix}pintarcek_
+├ ▪ _${usedPrefix}sadboycek_
+├ ▪ _${usedPrefix}sadgirlcek_
+├ ▪ _${usedPrefix}stresscek_
+├ ▪ _${usedPrefix}tololcek_
+├ ▪ _${usedPrefix}wibucek_
+├ ▪ _${usedPrefix}jadian_
+├ ▪ _${usedPrefix}mention_  [ user ]
+├ ▪ _${usedPrefix}say_  [ teks ]
+├ ▪ _${usedPrefix}bacot_  [ user ]
+├ ▪ _${usedPrefix}bucin_
+├ ▪ _${usedPrefix}motivasi_
+├ ▪ _${usedPrefix}pantun_
+├ ▪ _${usedPrefix}puisi_
+├ ▪ _${usedPrefix}syair_
+├ ▪ _${usedPrefix}quotes_
+├ ▪ _${usedPrefix}getaudio_  [ teks ]
+├ ▪ _${usedPrefix}getimg_  [ teks ]
+├ ▪ _${usedPrefix}getmsg_  [ teks ]
+├ ▪ _${usedPrefix}getsticker_  [ teks ]
+├ ▪ _${usedPrefix}getvideo_  [ teks ]
+├ ▪ _${usedPrefix}getvn_  [ teks ]
+├ ▪ _${usedPrefix}listaudio_  [ teks ]
+├ ▪ _${usedPrefix}listimg_  [ teks ]
+├ ▪ _${usedPrefix}listmsg_  [ teks ]
+├ ▪ _${usedPrefix}liststicker_  [ teks ]
+├ ▪ _${usedPrefix}listvideo_  [ teks ]
+├ ▪ _${usedPrefix}listvn_  [ teks ]
 └─────────
 `.trim()
   conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Menu Pengisi Kegabutan', 'status@broadcast')
