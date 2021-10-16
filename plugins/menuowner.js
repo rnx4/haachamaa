7de79e98b9592fbea79abe0c5860617bb2ff9aa3
unxzx <84166927+unx21@users.noreply.github.com>
2021-10-16 19:34:47 +0800

    Update menuowner.js

--- plugins/menuowner.js
+++ plugins/menuowner.js
@@ -4,30 +4,30 @@ let teks = `
 ┏━ *「 Menu Owner 」*
 ┃
 ┃
-┃ ⌲ _.addprems_  [ @user ]
-┃ ⌲ _.banchat_
-┃ ⌲ _.ban_  [ @user ]
-┃ ⌲ _.broadcast_  [ teks ]
-┃ ⌲ _.broadcastgroup_  [ teks ]
-┃ ⌲ _.broadcastjadibot_  [ teks ]
-┃ ⌲ _.creategroup_
-┃ ⌲ _.deletechat_
-┃ ⌲ _.deletechat group_
-┃ ⌲ _.mutechat_
-┃ ⌲ _.mutechat group_
-┃ ⌲ _.deleteallchat_
-┃ ⌲ _.deleteallchat group_
-┃ ⌲ _.muteallchat_
-┃ ⌲ _.muteallchat group_
-┃ ⌲ _.daftarulang_  [ @user ]
-┃ ⌲ _.delprem_  [ @user ]
-┃ ⌲ _.debounce_
-┃ ⌲ _.update_
+┃ ⌲ _${usedPrefix}addprems_  [ @user ]
+┃ ⌲ _${usedPrefix}banchat_
+┃ ⌲ _${usedPrefix}ban_  [ @user ]
+┃ ⌲ _${usedPrefix}broadcast_  [ teks ]
+┃ ⌲ _${usedPrefix}broadcastgroup_  [ teks ]
+┃ ⌲ _${usedPrefix}broadcastjadibot_  [ teks ]
+┃ ⌲ _${usedPrefix}creategroup_
+┃ ⌲ _${usedPrefix}deletechat_
+┃ ⌲ _${usedPrefix}deletechat group_
+┃ ⌲ _${usedPrefix}mutechat_
+┃ ⌲ _${usedPrefix}mutechat group_
+┃ ⌲ _${usedPrefix}deleteallchat_
+┃ ⌲ _${usedPrefix}deleteallchat group_
+┃ ⌲ _${usedPrefix}muteallchat_
+┃ ⌲ _${usedPrefix}muteallchat group_
+┃ ⌲ _${usedPrefix}daftarulang_  [ @user ]
+┃ ⌲ _${usedPrefix}delprem_  [ @user ]
+┃ ⌲ _${usedPrefix}debounce_
+┃ ⌲ _${usedPrefix}update_
 ┃ ⌲     _>_
 ┃ ⌲    _=>_
-┃ ⌲ _.kickall_
-┃ ⌲ _.leavegc_
-┃ ⌲ _.leavegroup_
+┃ ⌲ _${usedPrefix}kickall_
+┃ ⌲ _${usedPrefix}leavegc_
+┃ ⌲ _${usedPrefix}leavegroup_
 ┗ ┅ ━━━━━━━━━━
 `.trim()
     conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Menu nya Yang Punya Bot', 'status@broadcast')
