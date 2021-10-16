71c0cc51160b44559909793f03064d3b2905ee33
unxzx <84166927+unx21@users.noreply.github.com>
2021-10-16 19:57:52 +0800

    Update menuprems.js

--- plugins/menuprems.js
+++ plugins/menuprems.js
@@ -4,54 +4,54 @@ let teks = `
 ๑—————— *〔 Premium Menu 〕*
 ┊
 ┊
-┊ ⌬ _.addaudio_  [ teks ]
-┊ ⌬ _.addimg_  [ teks ]
-┊ ⌬ _.addmsg_  [ teks ]
-┊ ⌬ _.addsticker_  [ teks ]
-┊ ⌬ _.addvideo_  [ teks ]
-┊ ⌬ _.addvn_  [ teks ]
-┊ ⌬ _.delaudio_  [ teks ]
-┊ ⌬ _.delimg_  [ teks ]
-┊ ⌬ _.delmsg_  [ teks ]
-┊ ⌬ _.delsticker_  [ teks ]
-┊ ⌬ _.delvideo_  [ teks ]
-┊ ⌬ _.delvn_  [ teks ]
-┊ ⌬ _.fb_  [ url ]
-┊ ⌬ _.gift_
-┊ ⌬ _.glasses_
-┊ ⌬ _.healer_
-┊ ⌬ _.ig_  [ url ]
-┊ ⌬ _.igstalk_  [ username ]
-┊ ⌬ _.igstory_  [ username ]
-┊ ⌬ _.getcode_
-┊ ⌬ _.jadibot_
-┊ ⌬ _.stop_
-┊ ⌬ _.berhenti_
-┊ ⌬ _.join_  [ link whatsapp ]
-┊ ⌬ _.nsfwneko_
-┊ ⌬ _.nsftrap_
-┊ ⌬ _.nsfwaifu_
-┊ ⌬ _.overf_
-play_  [ judul musik ]
-┊ ⌬ _.pinterest_  [ pencarian ]
-┊ ⌬ _.shaunthesheep_  [ reply media ]
-┊ ⌬ _.start_
-┊ ⌬ _.next_
-┊ ⌬ _.leave_
-┊ ⌬ _.tiktok_  [ url ]
-┊ ⌬ _.twitter_  [ url ]
-┊ ⌬ _.uniform_
-┊ ⌬ _.wallp_  [ pencarian ]
-┊ ⌬ _.wallpaper_
-┊ ⌬ _.wm_  [ reply sticker ]
-┊ ⌬ _.wphen_
-┊ ⌬ _.wpnsfw_
-┊ ⌬ _.wpzettairyouiki
-┊ ⌬ _.yta_  [ url ]
-┊ ⌬ _.ytmp3_  [ url ]
-┊ ⌬ _.ytmp4_  [ url ]
-┊ ⌬ _.yts_  [ pencarian ]
-┊ ⌬ _.ytv_  [ url ]
+┊ ⌬ _${usedPrefix}addaudio_  [ teks ]
+┊ ⌬ _${usedPrefix}addimg_  [ teks ]
+┊ ⌬ _${usedPrefix}addmsg_  [ teks ]
+┊ ⌬ _${usedPrefix}addsticker_  [ teks ]
+┊ ⌬ _${usedPrefix}addvideo_  [ teks ]
+┊ ⌬ _${usedPrefix}addvn_  [ teks ]
+┊ ⌬ _${usedPrefix}delaudio_  [ teks ]
+┊ ⌬ _${usedPrefix}delimg_  [ teks ]
+┊ ⌬ _${usedPrefix}delmsg_  [ teks ]
+┊ ⌬ _${usedPrefix}delsticker_  [ teks ]
+┊ ⌬ _${usedPrefix}delvideo_  [ teks ]
+┊ ⌬ _${usedPrefix}delvn_  [ teks ]
+┊ ⌬ _${usedPrefix}fb_  [ url ]
+┊ ⌬ _${usedPrefix}gift_
+┊ ⌬ _${usedPrefix}glasses_
+┊ ⌬ _${usedPrefix}healer_
+┊ ⌬ _${usedPrefix}ig_  [ url ]
+┊ ⌬ _${usedPrefix}igstalk_  [ username ]
+┊ ⌬ _${usedPrefix}igstory_  [ username ]
+┊ ⌬ _${usedPrefix}getcode_
+┊ ⌬ _${usedPrefix}jadibot_
+┊ ⌬ _${usedPrefix}stop_
+┊ ⌬ _${usedPrefix}berhenti_
+┊ ⌬ _${usedPrefix}join_  [ link whatsapp ]
+┊ ⌬ _${usedPrefix}nsfwneko_
+┊ ⌬ _${usedPrefix}nsftrap_
+┊ ⌬ _${usedPrefix}nsfwaifu_
+┊ ⌬ _${usedPrefix}overf_
+┊ ⌬ _${usedPrefix}play_  [ judul musik ]
+┊ ⌬ _${usedPrefix}pinterest_  [ pencarian ]
+┊ ⌬ _${usedPrefix}shaunthesheep_  [ reply media ]
+┊ ⌬ _${usedPrefix}start_
+┊ ⌬ _${usedPrefix}next_
+┊ ⌬ _${usedPrefix}leave_
+┊ ⌬ _${usedPrefix}tiktok_  [ url ]
+┊ ⌬ _${usedPrefix}twitter_  [ url ]
+┊ ⌬ _${usedPrefix}uniform_
+┊ ⌬ _${usedPrefix}wallp_  [ pencarian ]
+┊ ⌬ _${usedPrefix}wallpaper_
+┊ ⌬ _${usedPrefix}wm_  [ reply sticker ]
+┊ ⌬ _${usedPrefix}wphen_
+┊ ⌬ _${usedPrefix}wpnsfw_
+┊ ⌬ _${usedPrefix}yta_  [ url ]
+┊ ⌬ _${usedPrefix}ytmp3_  [ url ]
+┊ ⌬ _${usedPrefix}ytmp4_  [ url ]
+┊ ⌬ _${usedPrefix}yts_  [ pencarian ]
+┊ ⌬ _${usedPrefix}ytv_  [ url ]
+┊ ⌬ _${usedPrefix}zettairyouiki
 ๑———————————————๑
 `.trim()
   conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', 'Menu Spesial Premium User', 'status@broadcast')
