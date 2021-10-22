62363092ef195b4cb780e6772b6b1029e72a71b4
Nathan <91236768+Sjsjix@users.noreply.github.com>
2021-10-22 13:34:57 +0700

    Update menumain.js

--- plugins/menumain.js
+++ plugins/menumain.js
@@ -1,4 +1,4 @@
-let handler = async (m) => {
+let handler  = async (m, { conn, usedPrefix }) => {
   
 let teks = `
 ╭─ *「 Menu Utama 」*
@@ -28,7 +28,7 @@ let teks = `
 │ - _${usedPrefix}daftar_  [ nama|umur ]
 │ - _${usedPrefix}register_  [ nama|umur ]
 │ - _${usedPrefix}tf_  [ uang @user ] Ⓛ
-│ - _${usedPrefix}transfer_  [ uang @user ] Ⓛ
+│ - _${usedPrefix}transfer_  [ uang @user ]
 │ - _${usedPrefix}unreg_  [ SN|Serial number ]
 │ - _${usedPrefix}unregister_  [ SN|Serial number ]
 ╰──────
