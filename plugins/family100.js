70bcae726eeb772372e1691ce901decbdee802ae
Nathan <91236768+Sjsjix@users.noreply.github.com>
2021-10-27 11:03:31 +0700

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -7,11 +7,11 @@ async function handler(m) {
         this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/family100', {}, 'APIKEY'))
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
+    *「 Family 100 」*
+    
 *Soal:* ${json.soal}
 
 Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
