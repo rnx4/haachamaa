a5fad10623c2daf4898038150db69cb92ce88835
Nathan <91236768+Sjsjix@users.noreply.github.com>
2021-10-27 11:09:25 +0700

    Update siapakahaku.js

--- plugins/siapakahaku.js
+++ plugins/siapakahaku.js
@@ -9,15 +9,12 @@ let handler  = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.siapakahaku[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/siapakahaku', {}, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/siapakahaku.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 *「 Siapakah Aku 」*
 
-${json.result.level}
-Soal: "${json.result.soal}"
+Soal: "${json.soal}"
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik *${usedPrefix}sahint* untuk bantuan
@@ -27,7 +24,7 @@ Bonus: Rp${poin}
       await conn.reply(m.chat, caption, m),
       json, poin,
       setTimeout(() => {
-        if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.siapakahaku[id][0])
+        if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.siapakahaku[id][0])
         delete conn.siapakahaku[id]
       }, timeout)
     ]
