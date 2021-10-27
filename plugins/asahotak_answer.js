2e073fbd81f6d3d241651eea1153b84ab347ed60
Nathan <91236768+Sjsjix@users.noreply.github.com>
2021-10-27 10:58:45 +0700

    Update asahotak_answer.js

--- plugins/asahotak_answer.js
+++ plugins/asahotak_answer.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.asahotak[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.asahotak[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.asahotak[id][2]
       m.reply(`*Benar!*\n+Rp${conn.asahotak[id][2]}`)
       clearTimeout(conn.asahotak[id][3])
       delete conn.asahotak[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
   return !0
