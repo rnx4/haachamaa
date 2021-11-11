1cd209dbe0fad51f3862d839ff55657edaa08021
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-11 21:38:09 +0800

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -3,7 +3,7 @@ let name = conn.getName(m.sender)
 let teks = ` _Hai ${name}_\n\n _Ada yang bisa saya bantu ?_`
 conn.fakeReply(m.chat, teks, '0@s.whatsapp.net', ' *Salam Hangat Dari Owner*', 'status@broadcast')
 }
-handler.customPrefix = /(B|b)ot/
+handler.customPrefix = /Bot/
 handler.command = new RegExp
 
 module.exports = handler
